92b5cce5c8070f4099497fde33dd88e2cacb3a9b
GOVIND KUMAR <39936384+govind2006@users.noreply.github.com>
2024-10-26 07:51:02 +0000

    firebase

---
+++ react-quiz/dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'react-quiz',
+  location: 'us-central1'
+};
+
